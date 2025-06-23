94fcd520e7e5d98e4f3d6628bdbb45dedd79681a
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-06-23 10:15:52 +0000

    Hieu

--- appQLBH/src/App.java
+++ appQLBH/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Worl!");
+        System.out.println("Hieu dep zai!!!");
+
+        test_NUMBER test = new test_NUMBER();
+
+        test.hieu();
     }
 }
