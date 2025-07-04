bebf26dd462d62585b2c1cd65b339f38a799cc7a
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-07-04 10:34:34 +0000

    cuu thang cu VIet ANh

--- appQLBH/src/App.java
+++ appQLBH/src/App.java
@@ -1,9 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hieu dep zai!!!");
+        User user = new User("Phan Trong Hieu", "hieudzvcl@gmail.com", "hieune1203");
+        System.out.println(user);
 
-        Flower test = new Flower();
-        test.testFlower();
+        user.setName("Hieu dep traiaaia");
+        user.setEmail("hieune@gmail.com");
 
+        System.out.println("Updated user info:");
+        System.out.println(user);
     }
 }
