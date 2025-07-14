2b1cf9d2dc500a41cae522e9fa2fdbe6d0a63595
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-07-14 10:53:35 +0000

    baigiuakiphan1

--- appQLBH/src/App.java
+++ appQLBH/src/App.java
@@ -1,19 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       KhachHang kh1 = new KhachHang ("PTH1", "PhanTrongHieu", "12345567");
-       KhachHang kh2 = new KhachHang ("PTH2", "PhanTrongHieu2", "123455678");
-
-       HangHoa hh1 = new HangHoa("HH1","Banh mi", "15000");
-       HangHoa hh2 = new HangHoa("HH2","Sua Chua", "20000");
-
+       String maKH = "PTH12";
+       String maHH = "HH2C";
        int soLuong = 10;
+       double donGia = 15000;
 
-       double thanhTien = soLuong * hh1.getDonGia();
-       GiaoDich gd1 = new GiaoDich(kh1.getMaKhachHang(), hh1.getMaHangHoa(), soLuong , thanhTien);
+       double thanhTien = soLuong * donGia;
 
-       System.out.println("Khach Hang: " + kh1);
-       System.out.println("Hang Hoa: " + hh1.getTenHangHoa() + " - Don gia: " + hh1.getDonGia);
-       System.out.println("Giao dich: " + gd1);
-
-    }
+       GiaoDich gd = new GiaoDich(maKH, maHH, soLuong, thanhTien);
+       
+       System.out.println(gd.toString());
 }
