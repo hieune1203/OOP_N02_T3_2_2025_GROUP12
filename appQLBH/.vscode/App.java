3b8f2f9713dc4a362ab855be149993aebda3d730
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-07-18 11:02:17 +0000

    giuakiphan2

--- appQLBH/.vscode/App.java
+++ appQLBH/.vscode/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD:appQLBH/src/App.java
-        TestGiaoDichApp.chayThu();
-=======
         TestProductApp.test();
->>>>>>> d46e7e155cb077fb5a86fb7fb4355b88f9f41a6b:appQLBH/.vscode/App.java
     }
 }
