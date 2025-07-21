bf9aa2d4fb2dbdc9e7baa79f7519ae7841ec9238
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-07-21 11:14:21 +0000

    giuakiphan2

---
+++ appQLBH/.vscode/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        TaoHangHoa taoHH = new TaoHangHoa();
+        
+        taoHH.taoMoiHangHoa(); 
+        taoHH.taoMoiHangHoa(); 
+        taoHH.hienThiDanhSach();
+    }
+}
