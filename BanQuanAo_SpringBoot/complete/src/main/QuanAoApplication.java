af75a089b8e1ee3c39643622abd6c0b87a6fc5ff
DaoDuyDong <24100085@st.phenikaa-uni.edu.vn>
2025-08-01 10:52:41 +0000

    1

---
+++ BanQuanAo_SpringBoot/complete/src/main/QuanAoApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanAoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanAoApplication.class, args);
+    }
+}
+
