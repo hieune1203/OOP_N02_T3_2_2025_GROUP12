e7f7c23a4d2879916cdba76b921fbe712f4a0740
DaoDuyDong <24100085@st.phenikaa-uni.edu.vn>
2025-08-01 11:02:25 +0000

    2

--- BanQuanAo_SpringBoot/complete/src/main/java/com/example/servingwebcontent/QuanAoApplication.java
+++ BanQuanAo_SpringBoot/complete/src/main/java/com/example/servingwebcontent/QuanAoApplication.java
@@ -1,4 +1,4 @@
-package com.example.quanao;
+package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
